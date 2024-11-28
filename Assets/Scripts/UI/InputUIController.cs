8c74df0f07d2c8dfa141840c7d9f6378cc66efd1
daniel.vilchis <daniel.vilchis@accenture.com>
2024-11-27 23:03:17 -0600

    button try

--- Assets/Scripts/UI/InputUIController.cs
+++ Assets/Scripts/UI/InputUIController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Fusion;
+using Fusion.XR.Shared.Locomotion;
+using JetBrains.Annotations;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class InputUIController : MonoBehaviour
 {
-
+    [SerializeField] RayBeamer rayBeamer;
   
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rayBeamer.onHitEnter.AddListener(OnBeamHitEnter);
     }
 
     // Update is called once per frame
@@ -20,9 +23,12 @@ public class InputUIController : MonoBehaviour
 
     }
 
-    public void ActionInputButton()
+    protected virtual void OnBeamHitEnter(Collider hitCollition, Vector3 position)
     {
-        
+        if (hitCollition.transform.CompareTag("Button"))
+        {
+            hitCollition.GetComponent<Button>().onClick.Invoke(); 
+        }
     }
 
 
